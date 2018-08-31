30512c7fa53f3853400f818bc7a280f2b4ae8317
Danilo E. F. Bento <ehrhardt@geocities.com>
2018-08-30 22:07:25 -0300

    It's interesting to look at my first python code (written at the day that I learned python) after almost 10 years and try to make it a little PEP8 compatible (pylint is grading it over 9, I'll stop for tonight).

--- Infraero.py
+++ Infraero.py
@@ -25,7 +25,7 @@ infra = Infraero.Harvester()
 flight = infra.request_flight('SBSP', 3100)
 airport = infra.request_airport('SBSP')
 '''
-__all__ = ['Harvester','Formater']
+__all__ = ['Harvester', 'Formater']
 
 import cookielib
 import urllib2
@@ -39,15 +39,16 @@ class RequestVarsHTMLParser(HTMLParser):
         self.requestvars = []
 
     def handle_starttag(self, tag, attrs):
-        if (tag == 'input'):
+        if tag == 'input':
             dic = dict(attrs)
             if dic.get('name') in ['__VIEWSTATE', '__EVENTVALIDATION']:
-                self.requestvars.append(tuple([dic.get('name'),dic.get('value')]))
-                
+                self.requestvars.append(tuple([dic.get('name'), dic.get('value')]))
+
     def get_request_vars(self):
         return self.requestvars
 
 class FlightDataHTMLParser(HTMLParser):
+    """Flight data parser"""
     def __init__(self):
         HTMLParser.__init__(self)
         self.openspan = ''
@@ -61,50 +62,52 @@ class FlightDataHTMLParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         self.openspan = ''
-        if (tag == 'span'):
+        if tag == 'span':
             dic = dict(attrs)
-            if (dic.has_key('id')):
-                if (dic.get('id').startswith('grd_voos_ctl') or (dic.get('id') == 'lbl_data_pagedate')):
+            if dic.has_key('id'):
+                if dic.get('id').startswith('grd_voos_ctl') or \
+                   (dic.get('id') == 'lbl_data_pagedate'):
                     self.openspan = dic.get('id')
 
-        elif (tag == 'tr'):
+        elif tag == 'tr':
             dic = dict(attrs)
-            if (dic.has_key('class')):
-                if (dic.get('class') == 'pagina'):
+            if dic.has_key('class'):
+                if dic.get('class') == 'pagina':
                     self.openpagination = True
 
-        elif (self.openpagination):
-            if (tag == "a"):
+        elif self.openpagination:
+            if tag == "a":
                 dic = dict(attrs)
-                if (dic.has_key('href')):
+                if dic.has_key('href'):
                     page = self.re_page.search(dic.get('href'))
-                    if (page != None):
+                    if page != None:
                         page = int(page.group(1))
-                        if (page > self.pagemax):
+                        if page > self.pagemax:
                             self.pagemax = page
 
     def handle_data(self, data):
-        if (self.openspan == 'lbl_data_pagedate'):
+        if self.openspan == 'lbl_data_pagedate':
             self.pagedate = data
-        elif (self.openspan != ''):
+        elif self.openspan != '':
             key = self.re_name.search(self.openspan).group(1).lower()
-            self.flightdata.append(tuple([key,data]))
-            if (self.openspan.startswith('grd_voos_ctl') and self.openspan.endswith('STATUS')):
+            self.flightdata.append(tuple([key, data]))
+            if self.openspan.startswith('grd_voos_ctl') and self.openspan.endswith('STATUS'):
                 self.flights.append(self.flightdata)
                 self.flightdata = []
-        if (self.openspan != ''):
+        if self.openspan != '':
             self.openspan = ''
 
     def get_flights(self):
         return self.flights
-    
+
     def get_date(self):
         return self.pagedate
-    
+
     def get_pages(self):
         return self.pagemax
 
-class Harvester:
+class Harvester(object):
+    """HTML spider"""
     def __init__(self):
         cookie = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
@@ -118,8 +121,8 @@ class Harvester:
     def get_pages(self):
         return self.pagemax
 
-    def request_session_page(self,url):
-        request = self.opener.open(url,urllib.urlencode(self.requestvars))
+    def request_session_page(self, url):
+        request = self.opener.open(url, urllib.urlencode(self.requestvars))
         data = request.read()
         parser = RequestVarsHTMLParser()
         parser.feed(data)
@@ -141,8 +144,9 @@ class Harvester:
 
         self.request_session_page('http://www.infraero.gov.br/voos/index.aspx')
 
-    def request_data_page(self,url):
-        request = self.opener.open(url,urllib.urlencode(self.requestvars))
+    def request_data_page(self, url):
+        """Crawls a flight data page"""
+        request = self.opener.open(url, urllib.urlencode(self.requestvars))
         data = request.read()
         parser = RequestVarsHTMLParser()
         parser.feed(data)
@@ -153,14 +157,15 @@ class Harvester:
         parser.feed(data)
         parser.close()
 
-        if (self.pagedate == ''):
+        if self.pagedate == '':
             self.pagedate = parser.get_date()
-        if (self.pagemax < parser.get_pages()):
+        if self.pagemax < parser.get_pages():
             self.pagemax = parser.get_pages()
 
-        return parser.get_flights()    
+        return parser.get_flights()
 
     def request_airport_pages(self, airport, page):
+        """Crawls pages of an airport"""
         argument = 'Page$' + str(page)
         self.requestvars.append(tuple(['ScriptManager1', 'update_grid|grd_voos']))
         self.requestvars.append(tuple(['__EVENTTARGET', 'grd_voos']))
@@ -173,10 +178,11 @@ class Harvester:
         return self.request_data_page('http://www.infraero.gov.br/voos/index_2.aspx')
 
     def request_flight(self, airport, flight):
+        """Returns information about an specific flight at an airport"""
         self.pagemax = 1
 
         flight = '' + str(flight)
-        while (len(flight) < 5):
+        while len(flight) < 5:
             flight = '0' + flight
 
         self.request_intermediate('hl_3')
@@ -186,13 +192,14 @@ class Harvester:
         self.requestvars.append(tuple(['opcoes', '0']))
         self.requestvars.append(tuple(['pode_pesquisar_aeroporto', 'S']))
         self.requestvars.append(tuple(['aeroportos_chegada_1', airport]))
-        self.requestvars.append(tuple(['txt_num_voo',flight]))
+        self.requestvars.append(tuple(['txt_num_voo', flight]))
         self.requestvars.append(tuple(['txt_water_box_ClientState', '']))
         self.requestvars.append(tuple(['btn_aeroporto', "Consultar V\xc3\xb4os"]))
 
         return self.request_data_page('http://www.infraero.gov.br/voos/index.aspx')
-    
+
     def request_airport(self, airport):
+        """Returns information about all flights at an airport"""
         self.pagemax = 1
 
         self.request_intermediate('hl_1')
@@ -207,44 +214,50 @@ class Harvester:
         data = self.request_data_page('http://www.infraero.gov.br/voos/index.aspx')
 
         i = 1
-        while (i < self.pagemax):
+        while i < self.pagemax:
             i = i+1
             data = data + self.request_airport_pages(airport, i)
 
         return data
 
-class Formater:
+class Formater(object):
+    """HTML output"""
     def __init__(self, flights):
+        """Constructor method receives the flights information"""
         self.flights = flights
 
+    #pylint: disable=no-self-use
     def print_table_row(self, flight, head):
-        if (head == True):
-            hd = 'h'
+        """Prints a flight row with flight information and
+        a boolean that set the row as a head row"""
+        if head:
+            headertype = 'h'
             index = 0
         else:
-            hd = 'd'
+            headertype = 'd'
             index = 1
 
         dic = dict(flight)
         color = 'black'
-        if (dic.get('status').lower() == 'cancelado'):
+        if dic.get('status').lower() == 'cancelado':
             color = 'red'
 
         print '<tr>'
         for data in flight:
-            print '<t' + hd + ' style="color: ' + color + '">'
+            print '<t' + headertype + ' style="color: ' + color + '">'
             print data[index]
-            print '</t' + hd + '>'
+            print '</t' + headertype + '>'
         print '</tr>'
 
-    def print_table(self, id):
+    def print_table(self, tableid):
+        """Prints a table with the specified id"""
         first = True
-        print '<table id="' + id + '">'
+        print '<table id="' + tableid + '">'
         for flight in self.flights:
             if first:
                 print '<thead>'
-                self.print_table_row(flight,True)
+                self.print_table_row(flight, True)
                 print '</thead><tbody>'
                 first = False
-            self.print_table_row(flight,False)
+            self.print_table_row(flight, False)
         print '</tbody></table>'
